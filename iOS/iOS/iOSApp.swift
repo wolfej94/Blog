662e3919b6bc086aba3371e259b79fee54aea494
James Wolfe <56296015+wolfej94@users.noreply.github.com>
2023-07-16 03:06:16 +0100

    Created iOS Project

---
+++ iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by James Wolfe on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
